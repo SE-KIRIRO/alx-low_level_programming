bece45ffee8efb6808c01c17b907f7b21c319821
lawrence <laurentiuslawrence@gmail.com>
2022-07-07 15:42:47 +0300

    more functions and nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: is the first integer
+ * @b: is the second integer
+ * Return: product of the multiplication
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -6,4 +6,6 @@ write a function that checks for uppercase character.
 1_ isdigit
 write a function that checks for a digit (0 through 9).
 
+2_ collaboration is multiplication
+write a function that multiplied two integers.
 
