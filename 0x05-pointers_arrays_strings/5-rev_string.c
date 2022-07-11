fae329e61bb4ac0a6b7bc02d2c2fbc1a71dbcb94
lawrence <laurentiuslawrence@gmail.com>
2022-07-11 20:46:51 +0300

    pointers arrays and strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,16 +9,19 @@
 void rev_string(char *s)
 {
 	int count;
+	int i;
 
+	i = 0;
 	count = 0;
 	while (*(s + count) != '\0')
 		count++;
 
-	while (*s != *(s + count))
+	while (*(s + i) != *(s + count))
 	{
-		char temp = *s;
-		*s = *(s + (count - 1));
-		*(s++ + (count - 1)) = temp;
+		char temp = *(s + i);
+		*(s + i) = *(s + (count - 1));
+		*(s + (count - 1)) = temp;
 		count--;
+		i++;
 	}
 }
