a1142888877634cb072293dcc9aa71923c2ce9d0
lawrence <laurentiuslawrence@gmail.com>
2022-07-12 20:36:28 +0300

    pointers arrays and strings

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strcpy - copies a string
+ * @dest: destination pointer
+ * @src: source pointer
+ * Return: char pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int count;
+	int i;
+
+	count = 0;
+	while (*(src + count) != '\0')
+		count++;
+	while (i < count)
+	{
+		*(dest + i) = *(src + i++);
+	}
+	*(dest + i) = '\0';
+	return (dest);
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -18,3 +18,7 @@ in this directory we dive into the world of pointers
 
 8_ write a function that prints n elements of an array of integers, followed by a new line.
 
+9_ write a function that copies the string pointed to by src, including the terminating null byte \0 to the buffer pointed to by dest.
+
+
+
