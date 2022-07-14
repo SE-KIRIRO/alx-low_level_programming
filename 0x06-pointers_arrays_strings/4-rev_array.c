031d0488dd5e50c6881163872752c0ee0e173b93
lawrence <laurentiuslawrence@gmail.com>
2022-07-14 15:08:47 +0300

    reversint integer array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,7 +3,7 @@
  * reverse_array - reverses the content of an array of integers
  * @a: pointer to an array
  * @n: the no of elements of the array
- * Return: void
+ * Return: nothing
  */
 void reverse_array(int *a, int n)
 {
@@ -13,7 +13,7 @@ void reverse_array(int *a, int n)
 	i = 0;
 	while (i < n--)
 	{
-		a[i] = temp;
+		temp = a[i];
 		a[i++] = a[n];
 		a[n] = temp;
 	}
