a0bd3d0663207a10408d418d9e8c894ece052ebe
lawrence <laurentiuslawrence@gmail.com>
2022-07-14 16:27:47 +0300

    capitalizing string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,41 +1,42 @@
 #include "main.h"
-int checkoprator(char c);
+int check_seperators(char c);
+
 /**
- * cap_string - capitalizes all words of a string
- * @s: pointer to input string
- * Return: pointer to capitalized string
+ * cap_string - a function that capitalizes all words of a string.
+ * @s: An input string to capitalize letters
+ * Return: pointer to s
  */
 char *cap_string(char *s)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
-	while (*s)
+	while (s[i])
 	{
 		if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
 			s[i] -= 32;
-		if (checkoperator(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
+
+		if (check_seperators(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
 			s[i + 1] -= 32;
 		i++;
 	}
+
 	return (s);
 }
 
 /**
- * checkoperator - checks for an operator
- * @c: character being checked
- * Return: integer
+ * check_seperators - Separators of words: space, tabulation, new line,
+ * ,, ;, ., !, ?, ", (, ), {, and }
+ * @c: an input character
+ * Return: 1 if seperator, 0 otherwise
  */
-int checkoperator(char c)
+int check_seperators(char c)
 {
-	int i;
-
-	i = 0;
-	char separators[13] = { ' ', '\t', '\n', ',', ';', '.',	'!', '?',
+	int i = 0;
+	char seperators[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?',
 		'"', '(', ')', '{', '}' };
 	for (; i < 13; i++)
 	{
-		if (c == separators[i])
+		if (c == seperators[i])
 			return (1);
 	}
 	return (0);
