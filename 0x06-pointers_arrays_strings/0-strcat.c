f5939c109cde7285df7ee53a01712f1dc8d34962
lawrence <laurentiuslawrence@gmail.com>
2022-07-14 08:47:04 +0300

    string concatenation

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @src: points to the source string
+ * @dest: points to the destination string
+ * Return: pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int d_cou;
+	int s_cou;
+
+	d_cou = 0;
+	while (*(dest + d_cou) != '\0')
+	{
+		d_cou++;
+	}
+	s_cou = 0;
+	while (*(src + s_cou) != '\0')
+	{
+		*(dest + (d_cou + s_cou)) = *(src + s_cou);
+		s_cou++;
+	}
+	*(dest + (d_cou + s_cou)) = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+in this directory i do more of pointers arrays and strings
+
+0- write a function that concatenates two strings.
+	this function appends the src string to the dest string, overwriting the terminating null byte at the end of dest and then adds a terminating null byte.
