9e42d4128592ecd6a92323d821af8859ec35afb8
lawrence <laurentiuslawrence@gmail.com>
2022-07-20 20:45:16 +0300

    recursion basics

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -5,7 +5,7 @@
  * value of x raised to the power of y
  * @x: is the integer being worked on
  * @y: is the integer that is the power
- * return: x raised to the power of y
+ * Return: x raised to the power of y
  */
 
 int _pow_recursion(int x, int y)
@@ -13,6 +13,6 @@ int _pow_recursion(int x, int y)
 	if (y == 0)
 		return (1);
 	if (y < 0)
-		return(-1);
-	return(x * _pow_recursion(x, (y-1)));
+		return (-1);
+	return (x * _pow_recursion(x, (y - 1)));
 }
