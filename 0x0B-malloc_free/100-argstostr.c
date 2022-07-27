7c52c28c708d1909b6127696fbd408755df966df
lawrence <laurentiuslawrence@gmail.com>
2022-07-27 21:29:25 +0300

    memory allocation

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,38 +1,44 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * print_tab - Prints an array of string
- * @tab: The array to print
- *
- * Return: nothing
+ * argstostr - A function that concatenates all arguments of your program
+ * @ac: number of arguments
+ * @av: array containing arguments
+ * Return: A pointer to string that containing all arguments
+ * or NULL if ac == 0, if av == NULL, or upon failure
  */
-void print_tab(char **tab)
+char *argstostr(int ac, char **av)
 {
-  int i;
+	char *new_str;
+	int len = 0, i = 0, j, k = 0;
 
-  for (i = 0; tab[i] != NULL; ++i)
-    {
-      printf("%s\n", tab[i]);
-    }
-}
+	if (ac <= 0 || av == NULL)
+		return (NULL);
 
-/**
- * main - check the code for Holberton School students.
- *
- * Return: 1 if an error occurred, 0 otherwise
- */
-int main(void)
-{
-  char **tab;
+	for (; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+			len++;
+		len++;
+	}
 
-  tab = strtow("      Holberton School         #cisfun      ");
-  if (tab == NULL)
-    {
-      printf("Failed\n");
-      return (1);
-    }
-  print_tab(tab);
-  return (0);
+	len++;
+	new_str = malloc(len * sizeof(char));
+	if (new_str == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			new_str[k] = av[i][j];
+			k++;
+		}
+		new_str[k] = '\n';
+		k++;
+	}
+
+	new_str[k] = '\0';
+	return (new_str);
 }
