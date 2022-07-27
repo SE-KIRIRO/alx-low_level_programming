37d7bdf5bea402eea7eb7f0b9da394d34f94e6e6
lawrence <laurentiuslawrence@gmail.com>
2022-07-27 09:10:19 +0300

    argument count and vector

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,10 +4,11 @@
  * main - prints the number of arguments passed into it
  * @argc: the argument count
  * @argv: the argument vector
+ * Return: the number of arguments
  */
 
 int main(int argc, char __attribute__((__unused))__ *argv[])
 {
-	printf("%d", argc);
+	printf("%d", argc - 1);
 	return (0);
 }
